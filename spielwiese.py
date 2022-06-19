cdbf1962cfb383eb295260ac4b5dccda3b2fa661
Polu Tartakower <polu.tartakower@gmx.de>
2022-06-19 22:17:40 +0200

    Weitere Auftrennung der Aufgaben

--- spielwiese.py
+++ spielwiese.py
@@ -1,5 +1,5 @@
 from datenstrukturen import Bruch
-from matheHelfer import berechneKehrwert, erweitereBruch
+from matheHelfer import berechneKehrwert, erweitereBruch, schreibeErweitern, schreibeKuerzen
 
 
 bruch_1 = Bruch(2,3)
@@ -9,3 +9,11 @@ print(bruch_2)
 bruch_1 = Bruch(2,5)
 kehrwert = berechneKehrwert(bruch_1)
 print(kehrwert)
+
+bruch = Bruch(2,3)
+formel = schreibeErweitern(bruch, 3)
+print(formel)
+
+bruch = Bruch(6,8)
+formel = schreibeKuerzen(bruch)
+print(formel)
