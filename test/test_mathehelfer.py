a0b520315410b39b55cf8f07375acd6426c10116
Polu Tartakower <polu@gmx.de>
2022-07-30 20:51:32 +0200

    Geänderte Aufgabenstellung

--- test/test_mathehelfer.py
+++ test/test_mathehelfer.py
@@ -12,8 +12,8 @@ def test_Bruch() -> None:
 def test_kuerzeBruch() -> None:
     assert Bruch(2,3) == mathehelfer.kuerzeBruch(Bruch(6,9))
     
-def test_wandleBruchZuDezimalzahl() -> None:
-    assert Dezimalzahl(0.75) == mathehelfer.wandleBruchZuDezimalzahl(Bruch(3,4))
+# def test_wandleBruchZuDezimalzahl() -> None:
+#     assert Dezimalzahl(0.75) == mathehelfer.wandleBruchZuDezimalzahl(Bruch(3,4))
 
 def test_erweitereBruch() -> None:
     assert Bruch(6,15) == mathehelfer.erweitereBruch(Bruch(2,5), 3)
@@ -27,8 +27,8 @@ def test_schreibeErweitern() -> None:
 def test_schreibeKuerzen() -> None:
     assert "<mfrac><mn>6</mn><mn>8</mn></mfrac><mo>=</mo><mfrac><mn>3</mn><mn>4</mn></mfrac>" == mathehelfer.schreibeKuerzen(Bruch(6,8))
 
-def test_vonBruchZuDezimalzahl() -> None:
-    assert "<mfrac><mn>3</mn><mn>4</mn></mfrac><mo>=</mo><mn>0.75</mn>" == mathehelfer.schreibeBruchZuDezimalzahl(Bruch(3,4))
+# def test_vonBruchZuDezimalzahl() -> None:
+#     assert "<mfrac><mn>3</mn><mn>4</mn></mfrac><mo>=</mo><mn>0.75</mn>" == mathehelfer.schreibeBruchZuDezimalzahl(Bruch(3,4))
 
-def test_vonDezimalzahlZuBruch() -> None:
-    assert "<mn>0.25</mn><mo>=</mo><mfrac><mn>25</mn><mn>100</mn></mfrac>" == mathehelfer.vonDezimalzahlZuBruch(Dezimalzahl(0.25))
+# def test_vonDezimalzahlZuBruch() -> None:
+#     assert "<mn>0.25</mn><mo>=</mo><mfrac><mn>25</mn><mn>100</mn></mfrac>" == mathehelfer.vonDezimalzahlZuBruch(Dezimalzahl(0.25))
